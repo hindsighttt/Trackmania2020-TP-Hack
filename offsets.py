2a5fb9aea995f316ba48d8e4448627c06dff4757
hindsighttt <131590753+hindsighttt@users.noreply.github.com>
2023-07-18 21:38:28 +0200

    Update offsets.py

--- offsets.py
+++ offsets.py
@@ -3,6 +3,7 @@ import pymem
 
 process = pymem.Pymem('Trackmania.exe')
 
+#base address resolving
 xbase = process.base_address + 0x01FD4FA0
 ybase = process.base_address + 0x01F4A380
 zbase = process.base_address + 0x01F7B850
@@ -10,6 +11,7 @@ zbase = process.base_address + 0x01F7B850
 xvelbase = process.base_address + 0x01F7B850
 zvelbase = process.base_address + 0x01F4A380
 
+#address offsets
 offsets_x = [0x30, 0x158, 0x58, 0x9D0, 0x38, 0x0, 0x78]
 offsets_y = [0x158, 0x58, 0xA78, 0xF0, 0x38, 0x0, 0x7C]
 offsets_z = [0x50, 0x130, 0x0, 0x9D0, 0x38, 0x0, 0x80]
